9386fc18d1556329733b4cb25e82c15f9ad3bb00
i582 <51853996+i582@users.noreply.github.com>
2025-08-07 23:00:16 +0400

    feat(func): backported FunC mapping changes

--- src/trace/func-mapping.ts
+++ src/trace/func-mapping.ts
@@ -1,3 +1,19 @@
+export enum FuncType {
+    INT = 172,
+    CELL = 173,
+    SLICE = 174,
+    BUILDER = 175,
+    CONT = 176,
+    TUPLE = 177,
+    TYPE = 178,
+}
+
+export enum FuncVarFlag {
+    IN = 1,
+    NAMED = 2,
+    TMP = 4,
+}
+
 /**
  * Represents a FunC source location.
  */
@@ -5,10 +21,34 @@ export type FuncSourceLoc = {
     readonly file: string
     readonly line: number
     readonly pos: number
-    readonly vars: undefined | string[]
+    readonly length: number
+    readonly vars: undefined | FuncVar[]
+    readonly stack_info: undefined | StackElementInfo[]
+    readonly stack_sub_info: Record<string, undefined | StackElementInfo[]>[]
     readonly func: string
     readonly first_stmt: undefined | boolean
     readonly ret: undefined | boolean
+}
+
+export type StackElementInfo = {
+    readonly name: string
+    readonly type_str: string
+    readonly value: string
+    readonly is_temporary: boolean
+    readonly copy_number: number
+    readonly flags: FuncVarFlag
+    readonly category: string
+    readonly source_info: string
+}
+
+/**
+ * Represents a FunC local variable descriptor.
+ */
+export type FuncVar = {
+    readonly name: string
+    readonly type: FuncType
+    readonly flags: number // FuncVarFlag
+    readonly value?: string
 }
 
 /**
