414a6d3adca223749bb56d167495833fe68900b9
i582 <51853996+i582@users.noreply.github.com>
2025-09-24 12:46:31 +0400

    fix: better `isLastInstruction` logic for code with DEBUGMARK, fix `skipRefs` mode exception for if recompilation

---
+++ src/runtime/test/compile.spec.ts
@@ -0,0 +1,44 @@
+import {compileCellWithMapping, decompileCell} from "../instr"
+import {parse, print} from "../../text"
+import * as fs from "node:fs"
+import {diffLines} from "diff"
+
+const test = (text: string): (() => void) => {
+    return () => {
+        const res = parse("test.asm", text)
+        if (res.$ === "ParseFailure") {
+            return
+        }
+
+        const [compiled] = compileCellWithMapping(res.instructions, {skipRefs: true})
+        expect(compiled.hash().toString("hex")).toMatchSnapshot()
+
+        const withoutDebugMarks = text
+            .split(/\n/)
+            .filter(line => !line.includes("DEBUGMARK"))
+            .join("\n")
+
+        const res2 = parse("test.asm", withoutDebugMarks)
+        if (res2.$ === "ParseFailure") {
+            return
+        }
+
+        const [compiled2] = compileCellWithMapping(res2.instructions, {skipRefs: true})
+        expect(compiled2.hash().toString("hex")).toMatchSnapshot()
+
+        const compiledAssembly = print(decompileCell(compiled))
+        const compiledAssembly2 = print(decompileCell(compiled2))
+
+        if (compiledAssembly !== compiledAssembly2) {
+            console.log(diffLines(compiledAssembly, compiledAssembly2))
+            expect(true).toBe(false)
+        }
+    }
+}
+
+describe("compileCell", () => {
+    it(
+        "should compile Vault with skipRefs option enabled",
+        test(fs.readFileSync(`${__dirname}/testdata/Vault.tasm`, "utf8")),
+    )
+})
